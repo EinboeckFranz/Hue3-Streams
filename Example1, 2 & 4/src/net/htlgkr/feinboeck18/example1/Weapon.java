eb6f268be38279cd35ddb3519a500441830d4a48
Franz Einböck <Einboeck.Franz@sus.htl-grieskirchen.at>
2020-10-20 10:17:38 +0200

    *Project ready for check* Fixed PrintAsTable

--- Example1, 2 & 4/src/net/htlgkr/feinboeck18/example1/Weapon.java
+++ Example1, 2 & 4/src/net/htlgkr/feinboeck18/example1/Weapon.java
@@ -8,20 +8,43 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Weapon {
-    private final String name;
-    private final CombatType combatType;
-    private final DamageType damageType;
-    private final int damage;
-    private final int speed;
-    private final int strength;
+    private String name;
+    private CombatType combatType;
+    private DamageType damageType;
+    private int damage;
+    private int speed;
+    private int strength;
     private int value;
     Printable printWeaponsNormal = weapons -> {
         List<Weapon> sortedWeapons = Weapon.sortListByCombatDamageName(weapons);
         sortedWeapons.forEach(System.out::println);
     };
     Printable printWeaponsAsTable = weapons -> {
+        int[] biggestValuesLength = getBiggestValueOfWeaponParameter(weapons);
+        printHeaderFooter(biggestValuesLength);
+        weapons.forEach(weapon -> {
+            System.out.print("\n| ");
 
+            printPart(biggestValuesLength[0], weapon.getName());
+            printPart(biggestValuesLength[1], weapon.getCombatType().toString());
+            printPart(biggestValuesLength[2], weapon.getDamageType().toString());
+            printPart(biggestValuesLength[3], weapon.getDamage()+"");
+            printPart(biggestValuesLength[4], weapon.getSpeed()+"");
+            printPart(biggestValuesLength[5], weapon.getStrength()+"");
+            printPart(biggestValuesLength[6], weapon.getValue()+"");
+        });
+        System.out.print("\n");
+        printHeaderFooter(biggestValuesLength);
     };
+
+    public Weapon() {
+        List<Weapon> weapons = readInWeapons("weapons.csv");
+        printWeaponsAsTable.print(weapons);
+    }
+
+    public static void main(String[] args) {
+        Weapon wpn = new Weapon();
+    }
 
     public Weapon(String name, CombatType combatType, DamageType damageType, int damage, int speed, int strength, int value) {
         this.name = name;
@@ -143,6 +166,34 @@ public class Weapon {
         weapons.sort(Comparator.comparing(Weapon::getCombatType).thenComparing(Weapon::getDamageType).thenComparing(Weapon::getName));
         return weapons;
     }
+
+    private int[] getBiggestValueOfWeaponParameter(List<Weapon> weapons) {
+        return new int[] {
+            weapons.stream().map(Weapon::getName).mapToInt(String::length).max().orElse(0),
+                weapons.stream().map(Weapon::getCombatType).mapToInt(value1 -> value1.toString().length()).max().orElse(0),
+                weapons.stream().map(Weapon::getDamageType).mapToInt(value1 -> value1.toString().length()).max().orElse(0),
+            weapons.stream().mapToInt(Weapon::getDamage).max().toString().toCharArray().length,
+            weapons.stream().mapToInt(Weapon::getSpeed).max().toString().toCharArray().length,
+            weapons.stream().mapToInt(Weapon::getStrength).max().toString().toCharArray().length,
+            weapons.stream().mapToInt(Weapon::getValue).max().toString().toCharArray().length
+        };
+    }
+
+    private void printHeaderFooter(int[] columnSizes) {
+        System.out.print("+");
+        Arrays.stream(columnSizes).forEach(column -> {
+            for (int i = 0; i < column+2; i++)
+                System.out.print("-");
+            System.out.print("+");
+        });
+    }
+
+    private void printPart(int length, String arg) {
+        System.out.print(arg);
+        for (int i = 0; i < (length - arg.toCharArray().length); i++)
+            System.out.print(" ");
+        System.out.print(" | ");
+    }
 }
 enum CombatType {
     MELEE,
@@ -153,7 +204,8 @@ enum DamageType {
     SLASHING,
     MISSILE,
     BLUNT,
-    PIERCING
+    PIERCING,
+    NONE
 }
 interface Printable {
     void print(List<Weapon> weapons);
